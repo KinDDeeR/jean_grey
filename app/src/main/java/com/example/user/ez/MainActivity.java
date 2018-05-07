b1178c9f0d2613a536d1b08f03fcce7077a52e4b
Anton Chyro <antonchyro@gmail.com>
2018-05-07 11:48:03 +0300

    AskinTask - ggwp

--- app/src/main/java/com/example/user/ez/MainActivity.java
+++ app/src/main/java/com/example/user/ez/MainActivity.java
@@ -1,15 +1,53 @@
 package com.example.user.ez;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.concurrent.TimeUnit;
+
+public class    MainActivity extends AppCompatActivity {
+
+    private TextView mInfoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ///hh
-        ///hjgj
+
+        mInfoTextView = (TextView) findViewById(R.id.textView);
+    }
+
+
+    public void onClick(View view) {
+        CatTask catTask = new CatTask();
+        catTask.execute();
+    }
+
+    class CatTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mInfoTextView.setText("Выполняется запрос ");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            mInfoTextView.setText("Выполнено!");
+        }
     }
 }
